edcd73be025eec2b2e31d3083adcf74d0aa6c0ca
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-01-02 10:26:11 +0900

    use symlink to share downloads directory.

--- container/home/yocto/build-yocto.sh
+++ container/home/yocto/build-yocto.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR/sources
 source ./meta-riscv/setup.sh
 
-#mkdir -p $SCRIPT_DIR/downloads
-#ln -sf   $SCRIPT_DIR/downloads
+mkdir -p $SCRIPT_DIR/downloads
+ln -sf   $SCRIPT_DIR/downloads
 
 MACHINE=qemuriscv64 bitbake core-image-full-cmdline
