5647331fcfdce5da503eec2c52328df235990012
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-02-04 20:06:31 +0900

    use default repo

--- clone.sh
+++ clone.sh
@@ -6,16 +6,6 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 HOST_DOCKER_HOME=$SCRIPT_DIR/container/home/yocto
 YOCTO_DIR=$HOST_DOCKER_HOME/sources
 
-# 'repo' command
-REPO_DIR=$SCRIPT_DIR/bin
-REPO=$REPO_DIR/repo
-
-# get 'repo'
-mkdir -p $REPO_DIR
-curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ${REPO}
-chmod a+x ${REPO}
-PATH=${REPO_DIR}:$PATH
-
 mkdir -p $YOCTO_DIR
 cd $YOCTO_DIR
 
